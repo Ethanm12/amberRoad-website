a463fbc8d91866d4bc0a7c6feb25672d8dec6d35
Ethan Morgan (Desktop) <ethanm@hotmail.co.nz>
2023-12-26 18:46:32 +0100

    Buttons replaced to Ul for easier Accessibility for screenreaders

--- src/components/headerNav.tsx
+++ src/components/headerNav.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const HeaderNav: React.FC = () => {
-    return (
-        <nav className='header-container'>
-            <div className='header-logo-container'>
-                <img src='/images/logo.svg' alt='logo' /> 
-            </div>
-            <div className='header-page-buttons-container'>
-                <button className='header-page-button'>Home</button>
-                <button className='header-page-button'>Contact</button>
-                <button className='header-page-button'>Pricing</button>
-                <button className='header-page-button'>Blog</button>
-            </div>
-            <div className='header-call-to-action-contianer'>
-                <button className='header-call-to-action-button'>Sign Up</button>
-                <button className='header-call-to-action-button'>Log In</button>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="header-container">
+      <div className="header-logo-container">
+        <img src="/images/logo.svg" alt="logo" />
+      </div>
+      <div className="header-page-buttons-container">
+        <ul>
+          <li className="header-page-button">Home</li>
+          <li className="header-page-button">Pricing</li>
+          <li className="header-page-button">Blog</li>
+          <li className="header-page-button">Contact</li>
+        </ul>
+      </div>
+      <div className="header-call-to-action-contianer">
+        <button className="header-call-to-action-button">Sign Up</button>
+        <button className="header-call-to-action-button">Log In</button>
+      </div>
+    </nav>
+  );
 };
 
 export default HeaderNav;
